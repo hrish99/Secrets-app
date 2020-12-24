900821deb50a5a7cea9d7f7453e695325b9efd89
hrish99 <hrishabhaswal99@gmail.com>
2020-12-24 16:36:32 +0530

    Passport js and Sessions

--- app.js
+++ app.js
@@ -1,70 +1,105 @@
-require('dotenv').config()
-const express = require("express")
-const bodyParser = require("body-parser")
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose')
-const encrypt = require("mongoose-encryption")
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
-const User = new mongoose.model('user', userSchema)
+const User = new mongoose.model("User", userSchema);
 
-app.get('/', function(req, res) {
-    res.render('home')
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
+    res.render("home");
 });
-app.get('/login', function(req, res) {
-    res.render('login')
+
+app.get("/login", function(req, res) {
+    res.render("login");
 });
 
-
-app.get('/register', function(req, res) {
-    res.render('register')
+app.get("/register", function(req, res) {
+    res.render("register");
 });
+
+app.get("/secrets", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function(req, res) {
-    const userName = req.body.username;
-    const userPassword = req.body.password;
 
-    const newUser = new User({
-        email: userName,
-        password: userPassword
-    });
-    newUser.save(function(err, result) {
-        if (!err) {
-            res.render('secrets')
-        } else {
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
             console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
-    })
+    });
+
 });
 
 app.post("/login", function(req, res) {
-    const username = req.body.username
-    const password = req.body.password
 
-    User.findOne({ email: username }, function(err, foundUser) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets')
-                }
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
-    })
-})
+    });
+
+});
+
 
 
 
@@ -72,5 +107,5 @@ app.post("/login", function(req, res) {
 
 
 app.listen(3000, function() {
-    console.log("Server at port 3000");
-})
+    console.log("Server started on port 3000.");
+});
